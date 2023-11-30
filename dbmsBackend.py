2c3a6d79cad539db254f44301582639f0fa14341
ravballrun <75504416+ravballrun@users.noreply.github.com>
2023-11-30 05:33:04 -0500

    Update dbmsBackend.py
    Creates functionality so user can add their data to create gardens in database and also fixes major bug that crashes site. This way connection is made in every function at the beginning. This solves the issue.

--- dbmsBackend.py
+++ dbmsBackend.py
@@ -8,19 +8,20 @@ import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+CORS(app,origins="http://127.0.0.1:5500")
 
-# Connect to MySQL
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Schwalbegraze0!",
-    database="gardenproject"
-)
-
-# Create a cursor object
-mycursor = mydb.cursor()
 
 def user_exists(email):
+    # Connect to MySQL
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     query = "SELECT COUNT(*) FROM User WHERE email = %s"
     values = (email,)
     mycursor.execute(query, values)
@@ -30,6 +31,15 @@ def user_exists(email):
 # Create
 @app.route('/register_user', methods=['POST'])
 def register_user():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     data = request.get_json()
 
     email = data.get('email')
@@ -51,6 +61,15 @@ def register_user():
 # Read
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     query = "SELECT * FROM User WHERE user_id = %s"
     values = (user_id,)
     mycursor.execute(query, values)
@@ -68,6 +87,15 @@ def get_user(user_id):
 # Update
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     data = request.get_json()
     query = "UPDATE User SET email = %s, password = %s WHERE user_id = %s"
     values = (data['email'], data['password'], user_id)
@@ -78,6 +106,15 @@ def update_user(user_id):
 # Delete
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     query = "DELETE FROM User WHERE user_id = %s"
     values = (user_id,)
     mycursor.execute(query, values)
@@ -87,6 +124,15 @@ def delete_user(user_id):
 # Add plant to the database
 @app.route('/add_plant', methods=['POST'])
 def add_plant():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     data = request.get_json()
 
     name = data.get('name')
@@ -108,7 +154,7 @@ def add_plant():
         return jsonify({"error": "Plant already exists"}), 400
     
     # Perform the database insertion here
-    query = "INSERT INTO plants (stage, health, name, type) VALUES (%s, %s, %s, %s)"
+    query = "INSERT INTO plants (name, type, stage, health) VALUES (%s, %s, %s, %s)"
     values = (name, type, stage, health)
     mycursor.execute(query, values)
     mydb.commit()
@@ -117,6 +163,15 @@ def add_plant():
 
 @app.route('/get_plants', methods=['GET'])
 def get_plants():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     query = "SELECT * FROM Plants"
     mycursor.execute(query)
     plants = mycursor.fetchall()
@@ -131,12 +186,21 @@ def get_plants():
             'health': plant[4]
         }
         plant_list.append(plant_data)
-    print(plant_list)
+    # print(plant_list)
 
     return jsonify(plant_list)
 
 @app.route('/group_plants_by', methods=['POST'])
 def group_plants_by():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     data = request.get_json()
     selected_column = data.get('column')
     print(selected_column)
@@ -146,10 +210,10 @@ def group_plants_by():
     try:
         # Assuming 'plants' is your table name
         query = f"SELECT {selected_column}, COUNT(plant_id) AS count FROM plants GROUP BY {selected_column}"
-        print(query)
+        # print(query)
         mycursor.execute(query)
         result = mycursor.fetchall()
-        print(result)
+        # print(result)
         grouped_data = [{"name": row[0], "count": row[1]} for row in result]
 
         return jsonify(grouped_data)
@@ -158,6 +222,15 @@ def group_plants_by():
         
 @app.route('/add_garden_worker', methods=['POST'])
 def add_garden_worker():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     data = request.get_json()
     query = "INSERT INTO workers (proficiency, name) VALUES (%s, %s)"
     values = (data['proficiency'], data['name'])
@@ -168,6 +241,15 @@ def add_garden_worker():
 # Read all
 @app.route('/get_all_garden_workers', methods=['GET'])
 def get_all_garden_workers():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
     query = "SELECT * FROM workers"
     mycursor.execute(query)
     garden_workers = mycursor.fetchall()
@@ -175,6 +257,117 @@ def get_all_garden_workers():
     workers_data = [{"id": row[0], "proficiency": row[1], "name": row[2]} for row in garden_workers]
     return jsonify(workers_data)
 
+@app.route('/gardens', methods=['POST'])
+def create_garden():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
+    data = request.get_json()
+
+    user_id = data.get('user_id')
+    location = data.get('location')
+    size = data.get('size')
+    capacity = data.get('capacity')
+    soil = data.get('soil')
+
+    # Validate that all required fields are provided
+    if not (user_id and location and size and capacity and soil):
+        return jsonify({"message": "All fields are required"}), 400
+
+    # Insert data into the ugardens table
+    query = "INSERT INTO ugardens (user_id, location, size, capacity, soil) VALUES (%s, %s, %s, %s, %s)"
+    values = (user_id, location, size, capacity, soil)
+
+    try:
+        mycursor.execute(query, values)
+        mydb.commit()
+        return jsonify({"message": "Garden added successfully"})
+    except Exception as e:
+        return jsonify({"message": f"Error: {str(e)}"}), 500
+    
+@app.route('/get_gardens', methods=['GET'])
+def get_gardens():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
+    query = "SELECT * FROM ugardens"
+    mycursor.execute(query)
+    gardens = mycursor.fetchall()
+
+    garden_list = []
+    for garden in gardens:
+        plant_data = {
+            'garden_id': garden[0],
+            'user_id': garden[1],
+            'location': garden[2],
+            'size': garden[3],
+            'capacity': garden[4],
+            'soil' : garden[5]
+        }
+        garden_list.append(plant_data)
+    # print(plant_list)
+
+    return jsonify(garden_list)
+
+@app.route('/get_user_gardens', methods=['POST'])
+def get_user_gardens():
+    # Get data from the request JSON
+    data = request.get_json()
+
+    # Extract user_id from the data
+    user_id = data.get('user_id')
+
+    # Establish a database connection
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Schwalbegraze0!",
+        database="new_gardenproject"
+    )
+
+    # Create a cursor object
+    mycursor = mydb.cursor()
+
+    # Execute the parameterized query
+    query = "SELECT user.email, ugardens.* FROM user JOIN ugardens ON user.user_id = ugardens.user_id WHERE user.user_id = %s"
+    mycursor.execute(query, (user_id,))
+    
+    # Fetch all results
+    user_gardens = mycursor.fetchall()
+
+    # Convert the results to a list of dictionaries
+    user_gardens_list = []
+    for user_garden in user_gardens:
+        user_garden_data = {
+            'email': user_garden[0],  # Assuming email is the first column in the user table
+            'garden_id': user_garden[1],  # Assuming user_id is the second column in the user table
+            # Add other columns from the user table as needed
+
+            'user_id': user_garden[2],  # Assuming garden_id is the first column in the ugardens table
+            'location': user_garden[3],  # Assuming location is the second column in the ugardens table
+            'size' : user_garden[4],
+            'capacity' : user_garden[5],
+            'soil' : user_garden[6]
+        }
+        user_gardens_list.append(user_garden_data)
+
+    # Close the database connection
+    mydb.close()
+
+    # Return the results as JSON
+    return jsonify(user_gardens_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
